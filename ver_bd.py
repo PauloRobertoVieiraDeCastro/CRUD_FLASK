36426703685b050e49cb7e8ce412a792a0dca1ef
Paulo Roberto Vieira de Castro <vieira.castro.1987@gmail.com>
2022-02-12 15:49:22 -0300

    Update ver_bd.py

--- ver_bd.py
+++ ver_bd.py
@@ -1,9 +1,9 @@
 import MySQLdb
 print('Conectando...')
-conn = MySQLdb.connect(user='root', passwd='cimento1', host='localhost', port=3306)
+conn = MySQLdb.connect(user='seu_usuario', passwd='suasenha', host='localhost', port=3306)
 cursor = conn.cursor()
 print(' -------------  Oleos:  -------------')
-cursor.execute('select * from oleoteca.oleo order by corrente asc')
+cursor.execute('select * from oleoteca.oleo order by corrente asc') #deixando em ordem alfabética
 for jogo in cursor.fetchall():
     print(jogo)
 conn.commit()
